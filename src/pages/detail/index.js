7e7160e6a9199cdf1ecf4b7fbbbe3834013f50f3
holdbiao <1007059411@qq.com>
2019-11-14 21:35:49 +0800

    增加组件懒加载

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { actionCreators } from './store'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import {
     DetailWrapper,
     Title,
@@ -32,4 +33,4 @@ const mapDispatch = (dispatch) => ({
     }    
 })
 
-export default connect(mapState, mapDispatch)(Detail)
+export default connect(mapState, mapDispatch)(withRouter(Detail))
